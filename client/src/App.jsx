5402cef7b6be27f30e893e1c2fae0ab7cce71fda
Navodya <it22608772@my.sliit.lk>
2024-04-02 20:28:24 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,21 @@ import Workerlist from './pages/Workerlist';
 import Addshift from './pages/Addshift';
 import Shiftlist from './pages/Shiftlist';
 import Staffmanagement from './pages/Staffmanagement';
+import Header from './component/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    {/* header */}
+    <Header/>
       <Routes>
+     
         <Route path="/" element={<Staffmanagement/>} />
         <Route path="/Addworkers" element={<Addworkers/>} />
         <Route path="/Workerlist" element={<Workerlist/>} />
         <Route path="/Addshift" element={<Addshift/>} />
         <Route path="/Shiftlist" element={<Shiftlist/>} />
+        
       </Routes>
     </BrowserRouter>
   );
